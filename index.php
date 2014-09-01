bcb57c1e9c9852eff266712ad4015eef3cb027a4
Murray MacLarty <maclarty@mweb.co.za>
2014-09-01 15:06:24 +0200

    Added license and index file

---
+++ index.php
@@ -0,0 +1,144 @@
+<?php require('assets/includes/inc_head_content.php');?>
+<div class="headerNav">
+	<div class="mainNav">
+		<ul>
+			<li><a class="scroll" href="#0">Home</a></li>
+			<li><a class="scroll" href="#1">Members</a></li>
+			<li><a class="scroll" href="#2">Contact Us</a></li>
+			<li><a class="scroll" href="#3">Else</a></li>
+		</ul>
+	</div>
+</div>
+<main>
+	<div id="0" class="cdFixedBg cdBg-1">
+		<div class="container">
+			<div class="logoContainer">
+				<h1>&lt;ohNoes&gt;</h1>
+				<h2>it's a clan website</h2>
+			</div>
+		</div>	
+	</div>
+
+	<div class="cdScrollingBg cdColor-1">
+		<div class="container">
+			<div class="cdContainer">
+				<div class="lachdanan">
+					<h2>Lachdanan's Scroll</h2>
+					<p>My name is Lachdanan, and I am cursed. Once the captain of King Leoric's army, I lived only to honor my land and my king. No man has a greater love for his king than I had for mine, even as I drove my blade through his dark and corrupted heart.</p>
+					<p>It was Lazarus - of that I am certain. He alone had the king's ear and whispered dark and evil magics into it, instilling the notion of an imminent attack by Westmarch. Afraid to speak against the archbishop, the councillors nodded their empty heads in agreement and sent us off to die.</p>
+					<p>When we returned from our horrific defeat in Westmarch, my beloved king lost all pertense of sanity. He seethed with rage, spitting curses upon us as traitors. With great sorrow I ran him through.</p>
+					<p>I will forever live in anguish for my last attempt to honor my king. As we lay him to rest in his burial chamber, he manifested as a hideous skeletal demon. Gorash and my other knights were overcome at once, but I fought on.</p>
+					<p>And now I wander, cursed by my once-beloved king. Evil gnaws at my bones, and I cannot risk putting my beloved Tristram in danger should I fail to contain that which tears at me. I must venture down into the labyrinth to die alone.</p>
+				</div>
+				<div class="leoric">
+					<h2>Leoric's Journal</h2>
+					<p>We have just arrived in Tristram, and I must say I'm a bit dismayed. This place is a backwater filled with serfs and an ancient broken down monastery, hardly fit for the king of Khanduras! I cannot fathom why Lazarus was so intent on this becoming our new seat of power.</p>
+					<p>A fetid, pallid malaise has fallen over the manor we now call home. Young Albrecht seems to be enjoying himself in our new home, however. Perhaps I am simply suffering from an imbalance of humors brought on by the recent change of clime.</p>
+					<p>I am convinced that some malevolent being is attempting to wrest control of my thoughts away from me. Voices direct me to horrendous acts, and there are times when I seem unable to control my body. Lazarus knows, this is certain. He looks at me strangely when he thinks me otherwise disposed.</p>
+					<p>Though my council begs me to reconsider, I will continue with the executions of those I find guilty of plotting against the kingdom. Perhaps they fear my eye will fall on them and discover their heinous, treasonous plans as well. Lazarus is the only one I can still trust.</p>
+					<p>I have finally rid myself of the dark influence seeking to subdue me, and now I see things as they truly are. This conspiracy among the insolent townsfolk to weaken me by stealing Albrecht away will not stand! Perhaps the heads of their women and children on pikes will bring them clarity...</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="1" class="cdFixedBg cdBg-2 scroll">
+
+		<div class="container">
+			<div class="logoContainer">
+				<h1 class="memberHeader">&lt;Members&gt;</h1>
+			</div>
+		</div>	
+	</div>
+
+	<div class="cdScrollingBg cdColor-2">
+		<div class="container">
+			<div class="cdContainer">
+				<div class="bioContainer">
+					<h3>Mou</h3>
+					<div class="row">
+						<div class="col two"><p>Do a flip! You, a bobsleder!? That I'd like to see! You, minion. Lift my arm. AFTER HIM! I guess if you want children beaten, you have to do it yourself.</p>
+						<p>You guys realize you live in a sewer, right? Enough about your promiscuous mother, Hermes! We have bigger problems. You are the last hope of the universe. Stop it, stop it. It's fine. I will 'destroy' you! Oh, I always feared he might run off like this. Why, why, why didn't I break his legs?</p>
+						<p>Oh sure! Blame the wizards! Ven ve voke up, ve had zese wodies. Wow, you got that off the Internet? In my day, the Internet was only used to download pornography. Negative, bossy meat creature! Then we'll go with that data file! Yes, I saw. You were doing well, until everyone died.</p>
+						<p>No! I want to live! There are still too many things I don't own! That's not soon enough! Wow! A superpowers drug you can just rub onto your skin? You'd think it would be something you'd have to freebase.</p></div>
+						<div class="col two"><img src="assets/images/leaderMonk.jpg" alt=""></div>
+					</div>
+				</div>
+				<div class="bioContainer">
+					<h3>Zee</h3>
+					<div class="row">
+						<div class="col two"><p>No! I want to live! There are still too many things I don't own! You seem malnourished. Are you suffering from intestinal parasites? Bender, you risked your life to save me! You're going back for the Countess, aren't you?</p>
+						<p>Oh, I don't have time for this.  I have to go and buy a single piece of fruit with a coupon and then return it, making people wait behind me while I complain. How much did you make me? I suppose I could part with 'one' and still be feared&hellip; No! The kind with looting and maybe starting a few fires!</p>
+						<p>Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo! These old Doomsday Devices are dangerously unstable. I'll rest easier not knowing where they are. When I was first asked to make a film about my nephew, Hubert Farnsworth, I thought "Why should I?" Then later, Leela made the film. But if I did make it, you can bet there would have been more topless women on motorcycles. Roll film! Oh sure! Blame the wizards! Is that a cooking show?</p>
+						<p>With gusto. You, minion. Lift my arm. AFTER HIM! Ah, the 'Breakfast Club' soundtrack!  I can't wait til I'm old enough to feel ways about stuff! Interesting. No, wait, the other thing: tedious. Hey, whatcha watching?</p></div>
+						<div class="col two"><img src="assets/images/zeeDH.jpg" alt=""></div>
+					</div>
+				</div>
+				<div class="bioContainer">
+					<h3>Mac</h3>
+					<div class="row">
+						<div class="col two"><p>Wow, you got that off the Internet? In my day, the Internet was only used to download pornography. As an interesting side note, as a head without a body, I envy the dead. Please, Don-Bot&hellip; look into your hard drive, and open your mercy file! You're going to do his laundry? That's not soon enough!</p>
+						<p>You've killed me! Oh, you've killed me! You're going to do his laundry? Guards! Bring me the forms I need to fill out to have her taken away!</p>
+						<p>Bender, hurry! This fuel's expensive! Also, we're dying! Your best is an idiot! Check it out, y'all. Everyone who was invited is here. With gusto.</p>
+						<p>No! I want to live! There are still too many things I don't own! But I know you in the future. I cleaned your poop. Oh no! The professor will hit me! But if Zoidberg 'fixes' it&hellip; then perhaps gifts! Do a flip! I suppose I could part with 'one' and still be feared&hellip;</p></div>
+						<div class="col two"><img src="assets/images/macWD.jpg" alt=""></div>
+					</div>
+				</div>
+				<div class="bioContainer">
+					<h3>DeathSlap</h3>
+					<div class="row">
+						<div class="col two"><p>Robot 1-X, save my friends!  And Zoidberg! I'll tell them you went down prying the wedding ring off his cold, dead finger. I suppose I could part with 'one' and still be feared&hellip; Wow, you got that off the Internet? In my day, the Internet was only used to download pornography.</p>
+						<p>Bender, you risked your life to save me! I'm a thing. The key to victory is discipline, and that means a well made bed. You will practice until you can make your bed in your sleep. Bender, hurry! This fuel's expensive! Also, we're dying! Fry, we have a crate to deliver. Do a flip!</p>
+						<p>Tell her you just want to talk. It has nothing to do with mating. Hi, I'm a naughty nurse, and I really need someone to talk to. $9.95 a minute. Is that a cooking show? I usually try to keep my sadness pent up inside where it can fester quietly as a mental illness. That's not soon enough! Have you ever tried just turning off the TV, sitting down with your children, and hitting them?</p>
+						<p>Oh, I don't have time for this.  I have to go and buy a single piece of fruit with a coupon and then return it, making people wait behind me while I complain. You're going back for the Countess, aren't you? Come, Comrade Bender! We must take to the streets! Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! Kif, I have mated with a woman. Inform the men.</p></div>
+						<div class="col two"><img src="assets/images/trevCS.jpg" alt=""></div>
+					</div>
+				</div>
+				<div class="bioContainer">
+					<h3>iCmaZ</h3>
+					<div class="row">
+						<div class="col two">
+							<div class="row">
+								<div class="col one"><span>Name: </span>Josh</div>
+							</div>
+							<div class="row">
+								<div class="col one"><span>Info: </span>Josh plays Starcraft.</div>
+							</div>
+
+						</div>
+						<div class="col two"><img src="assets/images/joshcat.jpg" alt=""></div>
+					</div>
+				</div>
+				<div class="bioContainer">
+					<h3>Harbinger</h3>
+					<div class="row">
+						<div class="col two"><p>You're going back for the Countess, aren't you? We're rescuing ya. Please, Don-Bot&hellip; look into your hard drive, and open your mercy file!</p>
+						<p>Kids have names? How much did you make me? Ask her how her day was. Take me to your leader! Bender, being God isn't easy. If you do too much, people get dependent on you, and if you do nothing, they lose hope. You have to use a light touch. Like a safecracker, or a pickpocket. And I'd do it again! And perhaps a third time! But that would be it.</p>
+						<p>I videotape every customer that comes in here, so that I may blackmail them later. Throw her in the brig. I suppose I could part with 'one' and still be feared&hellip; I saw you with those two "ladies of the evening" at Elzars. Explain that. The key to victory is discipline, and that means a well made bed. You will practice until you can make your bed in your sleep.</p>
+						<p>I'm a thing. You're going to do his laundry? Have you ever tried just turning off the TV, sitting down with your children, and hitting them? Negative, bossy meat creature! Your best is an idiot!</p></div>
+						<div class="col two"><img src="assets/images/harbWZ.jpg" alt=""></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="2" class="cdFixedBg cdBg-3">
+		<div class="container">
+			<div class="logoContainer">
+				<h1 class="contactHeader">&lt;Contact Us&gt;</h1>
+			</div>
+		</div>	
+	</div>
+
+	<div class="cdScrollingBg cdColor-3">
+		<div class="container">
+			<div class="cdContainer">
+				<p>Fuck you.</p>
+			</div>
+		</div>
+	</div>
+
+</main>
+
+<?php require('assets/includes/inc_scripts.php');?>
